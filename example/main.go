7f123052f0c4b5b5dc7adac841ad0ce72b1c019f
Jonsen Yang <im16hot@gmail.com>
2017-10-21 21:23:09 +0800

    Fix import path

--- example/main.go
+++ example/main.go
@@ -1,26 +1,27 @@
 package main
 
 import (
-    "fmt"
-    "flag"
-    "os"
-    "github.com/jonsen/gotld"
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/forease/gotld"
 )
 
 var url = flag.String("d", "", "check url")
 
 func main() {
-    flag.Parse()
+	flag.Parse()
 
-    if *url == "" {
-        fmt.Println("Usage: -d www.forease.net")
-        os.Exit(1)
-    }
+	if *url == "" {
+		fmt.Println("Usage: -d www.forease.net")
+		os.Exit(1)
+	}
 
-    tld, domain, err := gotld.GetTld( *url )
-    if err != nil {
-        fmt.Println( err )
-        return
-    }
-    fmt.Printf( "TLD: %s, Domain: %s\n", tld.Tld, domain )
+	tld, domain, err := gotld.GetTld(*url)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Printf("TLD: %s, Domain: %s\n", tld.Tld, domain)
 }
