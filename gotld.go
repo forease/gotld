c50ff635576b9458c36c08fbf13ffe68d11475ee
Jonsen Yang <im16hot@gmail.com>
2019-08-08 20:49:48 +0800

    1. 更TLD数据 2. 优化性能

--- gotld.go
+++ gotld.go
@@ -5,11 +5,13 @@
 package gotld
 
 import (
+	"bytes"
 	"errors"
 	"strings"
+	"sync"
 )
 
-// tld item
+// TldItem tld item
 type TldItem struct {
 	Id                     int32
 	Country, Tld, Category string
@@ -17,34 +19,84 @@ type TldItem struct {
 }
 
 const (
-	GOTLD_VERSION = "gotld V1.2"
+	GOTLD_VERSION = "gotld V1.3"
 )
 
-var tldMap = make(map[string]TldItem)
+var (
+	tldMap = make(map[string]TldItem)
+	pool   = sync.Pool{New: func() interface{} { return new(bytes.Buffer) }}
+)
 
 // Initialization Top Level Domain Table
 func init() {
 	initTld()
 }
 
-//
-//
+// GetTld 获取域名
 func GetTld(url string) (tld TldItem, domain string, err error) {
+	tld, domain, _, err = subDomain(url, 0)
+
+	return
+}
+
+// GetSubdomain 获取子域名
+func GetSubdomain(url string, level int) (subdomain, domain, tld string) {
+	var t TldItem
+	t, domain, subdomain, _ = subDomain(url, level)
+
+	return subdomain, domain, t.Tld
+}
+
+func subDomain(url string, level int) (tld TldItem, domain, subDomain string, err error) {
+
+	var (
+		buffer = pool.Get().(*bytes.Buffer)
+		isTLD  bool
+	)
 
 	dm := strings.Split(url, ".")
 
 	size := len(dm)
 	if size > 1 {
-		for i := 1; i < size; i++ {
-			value, ok := tldMap[strings.Join(dm[size-i:size], ".")]
+		idx := 0
+
+		for i := size - 1; i >= 0; i-- {
+			// 组合域名
+			for j := i; j < size; j++ {
+				buffer.WriteString(dm[j])
+				if j != size-1 {
+					buffer.WriteString(".")
+				}
+			}
+			subDomain = buffer.String()
+
+			// 重置buffer
+			buffer.Reset()
+
+			// 判断是否为TLD
+			value, ok := tldMap[subDomain]
 			if ok {
 				tld = value
-				domain = strings.Join(dm[size-i-1:size], ".")
+				isTLD = true
+				continue
+			}
+
+			// 找出TLD后
+			if isTLD {
+				if domain == "" {
+					domain = subDomain
+				}
+				if idx >= level {
+					break
+				}
+				idx++
 			}
 		}
 	} else {
 		tld, _ = tldMap[url]
 	}
+
+	pool.Put(buffer)
 
 	if tld.Tld == "" {
 		err = errors.New("Can't get tld from " + url)
@@ -52,9 +104,10 @@ func GetTld(url string) (tld TldItem, domain string, err error) {
 		tld.Lables = size
 	}
 
-	return tld, domain, err
+	return
 }
 
+// GetVersion -
 func GetVersion() string {
 	return GOTLD_VERSION
 }
